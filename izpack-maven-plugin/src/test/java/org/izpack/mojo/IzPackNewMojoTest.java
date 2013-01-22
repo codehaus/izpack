3ab9aac0cdfb044ac7052f4499005407b30e6edd
Mark Soderquist <mark@soderquist.net>
2013-01-21 20:12:24 -0700

    IZPACK-903 Installer file name has too many hyphens when generated with a Maven classifier

--- izpack-maven-plugin/src/test/java/org/izpack/mojo/IzPackNewMojoTest.java
+++ izpack-maven-plugin/src/test/java/org/izpack/mojo/IzPackNewMojoTest.java
@@ -20,57 +20,85 @@ import com.izforge.izpack.matcher.ZipMatcher;
 
 /**
  * Test of new IzPack mojo
- *
+ * 
  * @author Anthonin Bonnefoy
  */
-public class IzPackNewMojoTest extends AbstractMojoTestCase
-{
+public class IzPackNewMojoTest extends AbstractMojoTestCase {
 
     @Test
-    public void testExecute() throws Exception
-    {
-        File testPom = new File(
-                Thread.currentThread().getContextClassLoader().getResource("basic-pom.xml").toURI()
-        );
-        IzPackNewMojo mojo = (IzPackNewMojo) lookupMojo("izpack", testPom);
-        assertThat(mojo, IsNull.notNullValue());
-        initIzpackMojo(mojo);
+    public void testExecute() throws Exception {
+        File testPom = new File( Thread.currentThread().getContextClassLoader().getResource( "basic-pom.xml" ).toURI() );
+        IzPackNewMojo mojo = (IzPackNewMojo)lookupMojo( "izpack", testPom );
+        assertThat( mojo, IsNull.notNullValue() );
+        initIzpackMojo( mojo );
 
         mojo.execute();
 
-        File file = new File("target/sample/izpackResult.jar");
-        assertThat(file.exists(), Is.is(true));
-        JarFile jar = new JarFile(file);
-        assertThat((ZipFile)jar, ZipMatcher.isZipMatching(IsCollectionContaining.hasItems(
-            "com/izforge/izpack/core/container/AbstractContainer.class",
-            "com/izforge/izpack/uninstaller/Destroyer.class",
-            "com/izforge/izpack/panels/checkedhello/CheckedHelloPanel.class",
-            "META-INF/Test.png"
-        )));
+        File file = new File( "target/sample/izpackResult.jar" );
+        assertThat( file.exists(), Is.is( true ) );
+        JarFile jar = new JarFile( file );
+        assertThat( (ZipFile)jar, ZipMatcher.isZipMatching( IsCollectionContaining.hasItems( "com/izforge/izpack/core/container/AbstractContainer.class", "com/izforge/izpack/uninstaller/Destroyer.class", "com/izforge/izpack/panels/checkedhello/CheckedHelloPanel.class", "META-INF/Test.png" ) ) );
 
-        ZipEntry entry = jar.getEntry("META-INF/MANIFEST.MF");
-        InputStream content = jar.getInputStream(entry);
-        try
-        {
-            List<String> list = IOUtils.readLines(content);
-            assertThat(list, IsCollectionContaining.hasItem("SplashScreen-Image: META-INF/Test.png"));
-        }
-        finally
-        {
+        ZipEntry entry = jar.getEntry( "META-INF/MANIFEST.MF" );
+        InputStream content = jar.getInputStream( entry );
+        try {
+            List<String> list = IOUtils.readLines( content );
+            assertThat( list, IsCollectionContaining.hasItem( "SplashScreen-Image: META-INF/Test.png" ) );
+        } finally {
             content.close();
         }
 
     }
 
-    private void initIzpackMojo(IzPackNewMojo mojo) throws IllegalAccessException
-    {
-        File installFile = new File("target/test-classes/helloAndFinish.xml");
-        setVariableValueToObject(mojo, "comprFormat", "default");
-        setVariableValueToObject(mojo, "installFile", installFile.getAbsolutePath());
-        setVariableValueToObject(mojo, "kind", "standard");
-        setVariableValueToObject(mojo, "baseDir", new File("target/test-classes/").getAbsolutePath());
-        setVariableValueToObject(mojo, "output", "target/sample/izpackResult.jar");
-        setVariableValueToObject(mojo, "comprLevel", -1);
-        setVariableValueToObject(mojo, "mkdirs", true); // autoboxing
+    @Test
+    public void testFixIZPACK_903() throws Exception {
+        String classifier = "install";
+        File file = new File( "target/sample/izpackResult-" + classifier + ".jar" );
+
+        // Cleanup from any previous runs.
+        file.delete();
+        assertThat( file.exists(), Is.is( false ) );
+
+        // Create and configure the mojo.
+        File testPom = new File( Thread.currentThread().getContextClassLoader().getResource( "basic-pom.xml" ).toURI() );
+        IzPackNewMojo mojo = (IzPackNewMojo)lookupMojo( "izpack", testPom );
+        assertThat( mojo, IsNull.notNullValue() );
+        initIzpack5Mojo( mojo );
+
+        // In this case the classifier should be set.
+        setVariableValueToObject( mojo, "classifier", classifier );
+
+        // Execute the mojo.
+        mojo.execute();
+
+        // Ensure that the classifier value is still set correctly.
+        assertEquals( classifier, getVariableValueFromObject( mojo, "classifier" ) );
+
+        // Verify the generated file exists.
+        assertThat( file.exists(), Is.is( true ) );
     }
+
+    private void initIzpackMojo( IzPackNewMojo mojo ) throws IllegalAccessException {
+        File installFile = new File( "target/test-classes/helloAndFinish.xml" );
+        setVariableValueToObject( mojo, "comprFormat", "default" );
+        setVariableValueToObject( mojo, "installFile", installFile.getAbsolutePath() );
+        setVariableValueToObject( mojo, "kind", "standard" );
+        setVariableValueToObject( mojo, "baseDir", new File( "target/test-classes/" ).getAbsolutePath() );
+        setVariableValueToObject( mojo, "output", "target/sample/izpackResult.jar" );
+        setVariableValueToObject( mojo, "comprLevel", -1 );
+        setVariableValueToObject( mojo, "mkdirs", true ); // autoboxing
+    }
+
+    private void initIzpack5Mojo( IzPackNewMojo mojo ) throws IllegalAccessException {
+        File installFile = new File( "target/test-classes/helloAndFinish.xml" );
+        setVariableValueToObject( mojo, "comprFormat", "default" );
+        setVariableValueToObject( mojo, "installFile", installFile.getAbsolutePath() );
+        setVariableValueToObject( mojo, "kind", "standard" );
+        setVariableValueToObject( mojo, "baseDir", new File( "target/test-classes/" ).getAbsolutePath() );
+        setVariableValueToObject( mojo, "outputDirectory", new File( "target/sample" ).getAbsoluteFile() );
+        setVariableValueToObject( mojo, "finalName", "izpackResult" );
+        setVariableValueToObject( mojo, "comprLevel", -1 );
+        setVariableValueToObject( mojo, "mkdirs", true ); // autoboxing
+    }
+
 }
