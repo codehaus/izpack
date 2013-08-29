6f3d0222e586f7e8b9ae9fb2520c63ed9855f611
René Krell <renda.krell@gmail.com>
2013-08-29 18:09:20 +0200

    IZPACK-970 - post-fix with another approach to not break the existing API

--- izpack-panel/src/main/java/com/izforge/izpack/panels/userinput/validator/RegularExpressionValidator.java
+++ izpack-panel/src/main/java/com/izforge/izpack/panels/userinput/validator/RegularExpressionValidator.java
@@ -1,15 +1,15 @@
 /*
  * IzPack - Copyright 2001-2008 Julien Ponge, All Rights Reserved.
- * 
+ *
  * http://izpack.org/
  * http://izpack.codehaus.org/
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ public class RegularExpressionValidator implements Validator
 
     public static final String STR_PATTERN_DEFAULT = "[a-zA-Z0-9._-]{3,}@[a-zA-Z0-9._-]+([.][a-zA-Z0-9_-]+)*[.][a-zA-Z0-9._-]{2,4}";
 
-    private static final String PATTERN_PARAM = "pattern";
+    public static final String PATTERN_PARAM = "pattern";
 
     public boolean validate(ProcessingClient client)
     {
