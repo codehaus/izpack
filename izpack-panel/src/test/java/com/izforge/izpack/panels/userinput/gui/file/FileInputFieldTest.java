36ef4a197e31971db36504dfb037c8635ce5da47
Mark Heinze <mark.heinze@elster.com>
2014-03-03 14:46:35 -0500

    Fix formatting problem

--- izpack-panel/src/test/java/com/izforge/izpack/panels/userinput/gui/file/FileInputFieldTest.java
+++ izpack-panel/src/test/java/com/izforge/izpack/panels/userinput/gui/file/FileInputFieldTest.java
@@ -15,23 +15,23 @@ import com.izforge.izpack.panels.userinput.field.file.FileFieldView;
 
 public class FileInputFieldTest {
 
-	@Test
-	public void testEmptyFieldValidation() {
-		AbstractFileField field = Mockito.mock(AbstractFileField.class);
-		Mockito.when(field.getAbsoluteFile("")).thenReturn(new File("/"));
-		Mockito.when(field.getAllowEmptyValue()).thenReturn(Boolean.TRUE);
-		
-		IzPanel parent = Mockito.mock(IzPanel.class);
-		GUIInstallData installDataGUI = Mockito.mock(GUIInstallData.class);
-		Messages messages = Mockito.mock(Messages.class);
-		Mockito.when(installDataGUI.getMessages()).thenReturn(messages);
+    @Test
+    public void testEmptyFieldValidation()
+    {
+        AbstractFileField field = Mockito.mock(AbstractFileField.class);
+        Mockito.when(field.getAbsoluteFile("")).thenReturn(new File("/"));
+        Mockito.when(field.getAllowEmptyValue()).thenReturn(Boolean.TRUE);
+        
+        IzPanel parent = Mockito.mock(IzPanel.class);
+        GUIInstallData installDataGUI = Mockito.mock(GUIInstallData.class);
+        Messages messages = Mockito.mock(Messages.class);
+        Mockito.when(installDataGUI.getMessages()).thenReturn(messages);
 
-		FileFieldView view = new FileFieldView(field, null);
-		
-		FileInputField inputField = new FileInputField(view, parent, installDataGUI);
-		Assert.assertTrue(inputField.validateField());
-		
-		Assert.assertNull(inputField.getSelectedFile());
-	}
-
+        FileFieldView view = new FileFieldView(field, null);
+        
+        FileInputField inputField = new FileInputField(view, parent, installDataGUI);
+        Assert.assertTrue(inputField.validateField());
+        
+        Assert.assertNull(inputField.getSelectedFile());
+    }
 }
