fab5738435ba74b91dfbdc99ee1b1d8365113203
Miles Tjandrawidjaja <mtjandra@redhat.com>
2014-06-26 11:24:26 -0400

    Allow adding descriptions for file and directory fields. - This is useful when a file or directory field is dependent on some   condition. - Using a description means when you hide a file or directory field its   related text is also hidden

--- izpack-panel/src/main/java/com/izforge/izpack/panels/userinput/gui/file/AbstractGUIFileField.java
+++ izpack-panel/src/main/java/com/izforge/izpack/panels/userinput/gui/file/AbstractGUIFileField.java
@@ -104,6 +104,10 @@ public abstract class AbstractGUIFileField extends GUIField
     protected void init(FileInputField inputField)
     {
         this.fileInput = inputField;
+        if(getField().getDescription() != null)
+        {
+            addDescription();
+        }
         if (getField().getLabel() != null)
         {
             addLabel();
