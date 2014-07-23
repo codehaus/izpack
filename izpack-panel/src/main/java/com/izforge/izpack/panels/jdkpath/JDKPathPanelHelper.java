5748d15dd06bf38fdaafdc47da482cafeee1b908
Miles Tjandrawidjaja <mtjandra@redhat.com>
2014-07-23 14:03:18 -0400

    Cleanup of intial refactor of the JDKPathConsolePanel

--- izpack-panel/src/main/java/com/izforge/izpack/panels/jdkpath/JDKPathPanelHelper.java
+++ izpack-panel/src/main/java/com/izforge/izpack/panels/jdkpath/JDKPathPanelHelper.java
@@ -1,13 +1,106 @@
 package com.izforge.izpack.panels.jdkpath;
 
+import com.coi.tools.os.win.MSWinConstants;
+import com.izforge.izpack.api.exception.NativeLibException;
+import com.izforge.izpack.core.os.RegistryDefaultHandler;
+import com.izforge.izpack.core.os.RegistryHandler;
 import com.izforge.izpack.util.FileExecutor;
 import com.izforge.izpack.util.Platform;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class JDKPathPanelHelper
 {
+
+    /**
+     * Returns the path to the needed JDK if found in the registry. If there are more than one JDKs
+     * registered, that one with the highest allowed version will be returned. Works only on windows.
+     * On Unix an empty string returns.
+     *
+     * @return the path to the needed JDK if found in the windows registry
+     */
+    public static String getJavaHomeFromRegistry(RegistryDefaultHandler handler, String min, String max)
+    {
+        String javaHome = "";
+        int oldVal = 0;
+        RegistryHandler registryHandler = null;
+        Set<String> badRegEntries = new HashSet<String>();
+        try
+        {
+            // Get the default registry handler.
+            registryHandler = handler.getInstance();
+            if (registryHandler == null)
+            {
+                // We are on a os which has no registry or the
+                // needed dll was not bound to this installation. In
+                // both cases we forget the try to get the JDK path from registry.
+                return javaHome;
+            }
+
+            oldVal = registryHandler.getRoot(); // Only for security...
+            registryHandler.setRoot(MSWinConstants.HKEY_LOCAL_MACHINE);
+            String[] keys = registryHandler.getSubkeys(JDKPathPanel.JDK_ROOT_KEY);
+            if (keys == null || keys.length == 0)
+            {
+                return javaHome;
+            }
+            Arrays.sort(keys);
+            int i = keys.length - 1;
+
+            // We search for the highest allowed version, therefore retrograde
+            while (i > 0)
+            {
+                String javaVersion = extractJavaVersion(keys[i]);
+                if (max == null || compareVersions(javaVersion, max, false))
+                {
+                    // First allowed version found, now we have to test that the min value
+                    // also allows this version.
+                    if (min == null || compareVersions(javaVersion, min, true))
+                    {
+                        String cv = JDKPathPanel.JDK_ROOT_KEY + "\\" + keys[i];
+                        String path = registryHandler.getValue(cv, JDKPathPanel.JDK_VALUE_NAME).getStringData();
+                        // Use it only if the path is valid.
+                        // Set the path for method JDKPathPanelHelper.pathIsValid ...
+                        if (!JDKPathPanelHelper.pathIsValid(path))
+                        {
+                            badRegEntries.add(keys[i]);
+                        }
+                        else
+                        {
+                            javaHome = path;
+                            break;
+                        }
+                    }
+                }
+                i--;
+            }
+        }
+        catch (Exception e)
+        { // Will only be happen if registry handler is good, but an
+            // exception at performing was thrown. This is an error...
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (registryHandler != null && oldVal != 0)
+            {
+                try
+                {
+                    registryHandler.setRoot(MSWinConstants.HKEY_LOCAL_MACHINE);
+                }
+                catch (NativeLibException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return javaHome;
+    }
+
     /**
      * Returns whether the chosen path is true or not. If existFiles are not null, the existence of
      * it under the chosen path are detected. This method can be also implemented in derived
@@ -28,37 +121,51 @@ public class JDKPathPanelHelper
         return true;
     }
 
-    public static boolean verifyVersion(String vs, String min, String max)
+    /**
+     * Validate that the given javaVersion meets meets the minimum and maximum java version requirements.
+     *
+     * @param javaVersion
+     * @param min
+     * @param max
+     * @return
+     */
+    public static boolean verifyVersion(String javaVersion, String min, String max)
     {
-        boolean retval = true;
+        boolean valid = true;
+
         // No min and max, version always ok.
         if (min == null && max == null)
         {
-            return (true);
+            return true;
         }
 
         if (min != null)
         {
-            if (!compareVersions(vs, min, true))
+            if (!compareVersions(javaVersion, min, true))
             {
-                retval = false;
+                valid = false;
             }
         }
         if (max != null)
         {
-            if (!compareVersions(vs, max, false))
+            if (!compareVersions(javaVersion, max, false))
             {
-                retval = false;
+                valid = false;
             }
         }
-        return retval;
+        return valid;
     }
 
+    /**
+     * Run the java binary from the JAVA_HOME directory specified from the user.
+     * We do this to figure out what version of java the user has specified
+     *
+     * @param path JAVA_HOME
+     * @param platform specifies which platform user is running installation on
+     * @return string representation of the java version
+     */
     public static String getCurrentJavaVersion(String path, Platform platform)
     {
-        // Now get the version ...
-        // We cannot look to the version of this vm because we should
-        // test the given JDK VM.
         String[] params;
         if (platform.isA(Platform.Name.WINDOWS))
         {
@@ -76,19 +183,31 @@ public class JDKPathPanelHelper
                     "-version"
             };
         }
+
         String[] output = new String[2];
         FileExecutor fe = new FileExecutor();
         fe.executeCommand(params, output);
 
-        // "My" VM writes the version on stderr :-(
-        String vs = (output[0].length() > 0) ? output[0] : output[1];
-        String javaVersion = getCurrentJavaVersion(vs, 4, 4, "__NO_NOT_IDENTIFIER_");
-        return javaVersion;
+        // Get version information from stdout or stderr, may vary across machines
+        String versionInformation = (output[0].length() > 0) ? output[0] : output[1];
+        return extractJavaVersion(versionInformation);
     }
 
-     public static String getCurrentJavaVersion(String in, int assumedPlace, int halfRange, String useNotIdentifier)
+    /**
+     * Given a 'dirty' string representing the javaVersion.
+     * Extract the actual java version and strip away any extra information.
+     *
+     * @param javaVersion
+     * @return
+     */
+     public static String extractJavaVersion(String javaVersion)
      {
-         StringTokenizer tokenizer = new StringTokenizer(in, " \t\n\r\f\"");
+         //Were originally parameters
+         int assumedPlace = 4;
+         int halfRange = 4;
+         String useNotIdentifier = "__NO_NOT_IDENTIFIER_";
+
+         StringTokenizer tokenizer = new StringTokenizer(javaVersion, " \t\n\r\f\"");
          int i;
          int currentRange = 0;
          String[] interestedEntries = new String[halfRange + halfRange];
@@ -128,9 +247,16 @@ public class JDKPathPanelHelper
          return interestedEntries[i];
      }
 
-     public static boolean compareVersions(String currentVersion, String template, boolean isMin)
-    {
-
+    /**
+     * Validate that the given javaVersion meets meets the minimum and maximum java version requirements.
+     *
+     * @param currentVersion
+     * @param template
+     * @param isMin
+     * @return
+     */
+     private static boolean compareVersions(String currentVersion, String template, boolean isMin)
+     {
         StringTokenizer currentTokenizer = new StringTokenizer(currentVersion, "._-");
         StringTokenizer neededTokenizer = new StringTokenizer(template, "._-");
         while (neededTokenizer.hasMoreTokens())
@@ -168,19 +294,19 @@ public class JDKPathPanelHelper
             {
                 if (isMin)
                 {
-                    return (false);
+                    return false;
                 }
-                return (true);
+                return true;
             }
-            if (currentValue > neededValue)
+            else if (currentValue > neededValue)
             {
                 if (isMin)
                 {
-                    return (true);
+                    return true;
                 }
-                return (false);
+                return false;
             }
         }
-        return (true);
+        return true;
     }
 }
